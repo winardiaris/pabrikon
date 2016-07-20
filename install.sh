3eaa8fcd278879f1097625cf22c58dbbcee948f1
Aris Winardi <me@arwin.my.id>
2016-07-20 18:28:11 +0700

    ubah dikit

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ unzip master.zip
 mkdir -p $pwd/$NAMA
 cp -r pabrik-ikon-master/data $pwd/$NAMA/data
 cp pabrik-ikon-master/index.theme $pwd/$NAMA
-cp pabrik-ikon-master/skrip.sh $pwd/$NAMA
+cp pabrik-ikon-master/pabrik.sh $pwd/$NAMA
 cd $pwd/$NAMA
 mkdir -p {actions,animations,apps,categories,devices,emblems,io,mimetypes,places,status,stock}/scalable
-sed 's/nama-ikon/$NAMA/g ;s/komentar/$KOMENTAR/g' index.theme > index.themes
+sed "s/nama-ikon/$NAMA/g ;s/komentar/$KOMENTAR/g" index.theme > index.themes
 rm -rf index.theme
 mv index.themes index.theme
 

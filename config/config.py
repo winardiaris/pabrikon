1425532a731f823989e594a9986cb5d1cc3a3cc7
Aris Winardi <me@arwin.my.id>
2016-08-25 18:12:09 +0700

    add list icon size

--- config/config.py
+++ config/config.py
@@ -4,6 +4,9 @@ csv_editor = 'libreoffice --calc'
 # directory
 list_dirs = ['actions','animations','apps','categories','devices','emblems','io','mimetypes','places','status','stock']
 
+# icon size
+icon_sizes = ['16','22','24','32','48','64','96']
+
 
 # Version
 version = '0.2-dev-gui'

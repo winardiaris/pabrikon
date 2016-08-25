d561776631bf69a58b3e4d3f3cbfea7ccfbbb8cb
Aris Winardi <me@arwin.my.id>
2016-08-25 11:49:41 +0700

    add config

---
+++ config/config.py
@@ -0,0 +1,9 @@
+# csv editor
+csv_editor = 'libreoffice --calc'
+
+# directory
+list_dirs = ['actions','animations','apps','categories','devices','emblems','io','mimetypes','places','status','stock']
+
+
+# Version
+version = '0.2-dev-gui'

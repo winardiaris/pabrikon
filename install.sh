97da42908baf8a24e2a083b823717ca96260f144
Aris Winardi <me@arwin.my.id>
2016-09-07 13:12:26 +0700

    fix typo

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cd /tmp
 wget https://github.com/winardiaris/pabrik-ikon/archive/dev-gui.zip
 unzip dev-gui.zip
-sudo move pabrik-ikon-dev-gui /opt/pabrik-ikon
+sudo mv pabrik-ikon-dev-gui /opt/pabrik-ikon
 sudo ln -s /opt/pabrik-ikon/bin/pabrik.py /usr/local/bin/pabrik
 echo "[info] instalation complete"
 echo "type 'pabrik -h' for help the pabrik-ikon "

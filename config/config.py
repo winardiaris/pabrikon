82c387674fd6c202afbef2874fede42b779fdedc
Aris Winardi <ariswinardy@gmail.com>
2016-09-23 15:34:20 +0700

    add new size

--- config/config.py
+++ config/config.py
@@ -2,10 +2,10 @@
 csv_editor = 'libreoffice --calc'
 
 # directory
-list_dirs = ['actions','animations','apps','categories','devices','emblems','io','mimetypes','places','status','stock']
+list_dirs = ['actions','animations','apps','categories','devices','emblems','emotes','io','mimetypes','places','status','stock']
 
 # icon size
-icon_sizes = ['16','22','24','32','48','64','96']
+icon_sizes = ['16','22','24','32','48','64','96','128']
 
 
 # Version

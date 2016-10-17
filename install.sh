83b3df86947f7885473a2c6aa8fb0de772868ed5
Aris Winardi <ariswinardy@gmail.com>
2016-10-17 15:35:07 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cd /tmp
-wget https://github.com/winardiaris/pabrikon/archive/master.zip
-unzip master.zip
-sudo mv pabrikon-master /opt/pabrikon
+sudo git clone https://github.com/winardiaris/pabrikon.git /opt/pabrikon
 sudo ln -s /opt/pabrikon/bin/pabrikon.py /usr/local/bin/pabrikon
 echo "[info] instalation complete"
 echo "type 'pabrikon -h' for help the pabrikon "
